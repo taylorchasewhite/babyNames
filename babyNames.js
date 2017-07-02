0f5c0c25532e53d6704671e662b5fd004a17eda0
Taylor White <taylor@taylorchasewhite.com>
2017-07-02 17:21:02 -0500

    Add government data from Social Security Administration
    Add the csv documents from SSA.open.data, pull the years available in
    the dataset.

--- babyNames.js
+++ babyNames.js
@@ -10,11 +10,39 @@ function babyNamesInitialize(elementID) {
 	else {
 		parentDivID="divBabyNames";
 	}
+	loadYearsData();
 	loadBabyNameData();
 }
 
+function loadYearsData() {
+	d3.tsv("./data/years.tsv",function (error,data) {
+		if (error) {
+			throw error;
+		}
+		
+		data.sort( function (a,b) {
+			return d3.descending(a.Year,b.Year);
+		});
+		
+		var containingDiv=d3.selectAll("body").insert("div",":nth-child(2)");
+		containingDiv.text("Show popularity of birth name on ");
+		var select=containingDiv.insert("select");
+		select.attr("id","selYear");
+		select.selectAll('option')
+		.data(data)
+		.enter()
+		.append('option')
+		.text(function(d) {
+			return d.Year;
+		})
+		.attr('value',function(d) {
+			return d.Year;
+		});
+	})
+}
+
 function loadBabyNameData() {
-	d3.tsv("./babyNames.tsv",function(error,data) {
+	d3.tsv("./data/babyNames.tsv",function(error,data) {
 		if (error) {
 			throw error;
 		}
@@ -49,4 +77,14 @@ function bindBabyNamesToDOM(babyData) {
 			return "both";	
 		}
 	});
+}
+
+function addSelectHandler() {
+	var yearSelect = $("#selYear");
+	
+	yearSelect.change(function() {
+		//generateDropdown(appSelect.val());
+		window.location.href = "http://taylorchasewhite.com/d3/nabu/Shared/?app="+yearSelect.val();
+	});
+	
 }
