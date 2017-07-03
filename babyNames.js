6985c205a559a3296c1967168db3857e4b9c2ff6
Taylor White <taylor@taylorchasewhite.com>
2017-07-02 22:15:53 -0500

    Bubble Chart for most frequently used names

--- babyNames.js
+++ babyNames.js
@@ -43,16 +43,80 @@ function loadBabyNameData(parentDivID,path) {
 			return d3.ascending(a.Name,b.Name);
 		});
 		babyNameData=data;
-		bindBabyNamesToDOM(data,parentDivID);
+		//bindBabyNamesToDOM(data,parentDivID);
+		generateBabyBubbleChart(data,parentDivID);
 	});
+}
+
+function generateBabyBubbleChart(babyData,parentDivID) {
+	if (parentDivID!=="divAllBabyNames") {
+		return;
+	}
+	var newData=[];
+	var maxBirths=d3.max(babyData, function(d) {
+		return +d.BirthCount;
+	});
+	babyData.forEach(function(d,i) {
+		if (d.BirthCount >= 0.3*maxBirths) {
+			newData.push(d);
+		}
+	});
+	
+	var chart = bubbleChart().width(960).height(960).minRadius(7).maxRadius(55).forceApart(-170);
+	chart.columnForColors("Sex").columnForRadius("BirthCount").unitName("babies").columnForTitle("Name");
+	chart.customColors(["M","F"],["#70b7f0","#e76486"]).showTitleOnCircle(true);
+	console.log(chart.showTitleOnCircle());
+	d3.select("#"+parentDivID)
+		.data(newData)
+		.call(chart);
+	return;
+	
+	var width = 1920,
+		height = 1920;
+	var svg = d3.select("body").append("svg")
+		.attr("height",height)
+		.attr("width",width);
+
+	var format = d3.format(",d");
+	var scaleRadius = d3.scaleLinear()
+		.domain([d3.min(data, function(d) { return +d.BirthCount; }),
+			d3.max(data, function(d) { return +d.BirthCount; })])
+		.range([5,18]);
+	
+	//var color = d3.scaleOrdinal(d3.schemeCategory20c);
+	
+	svg
+	.selectAll("circle")
+	.data(babyData)
+	.enter()
+	.append("circle")
+	.attr("r",function(d) {
+		return scaleRadius(d.BirthCount);
+	})
+	.attr("class",function(d) {
+		return getSexClass(d);
+	})
+	.on("mouseover", function(d){
+		tooltip.html(d.Name +"<br>"+ getSexClass(d.Sex) +"<br>"+d.BirthCount);
+		return tooltip.style("visibility", "visible");
+	})
+    .on("mousemove", function(){
+		return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
+	})
+	.on("mouseout", function(){return tooltip.style("visibility", "hidden");
+});
+	
 }
 
 function bindBabyNamesToDOM(babyData,parentDivID) {
 	if(!babyData) {
 		babyData=babyNameData;
 	}
-	var list=d3.select("#"+parentDivID).append("ul");
 	
+	/*var rscale = d3.scaleLinear()*/
+	
+	var list=d3.select("#"+parentDivID).append("ul");
+
 	list.selectAll("li")
 	.data(babyData)
 	.enter()
@@ -61,22 +125,15 @@ function bindBabyNamesToDOM(babyData,parentDivID) {
 		return d.Name;
 	})
 	.attr("class",function(d) {
-		if (d.Sex==="Girl" || d.Sex==="F") {
-			return "girl";
-		}
-		else if (d.Sex==="Boy" || d.Sex==="M") {
-			return "boy";
-		}
-		else {
-			return "both";	
-		}
+		return getSexClass(d);
 	});
 }
 
 function removeBabyNamesFromDOM(parentDivID) {
 	d3.select("#"+parentDivID)
-		.selectAll("ul").remove();	
-	console.log("removed");
+		.selectAll("ul").remove();
+	var svg=d3.select("#"+parentDivID).select("svg");
+	svg.selectAll("g").remove();
 }
 
 function loadYearsData() {
@@ -141,4 +198,16 @@ function onYearChange() {
 	parentDivInfo=generationInfo;
 	console.log(parentDivInfo);
 	genMultipleBabyNameLists(parentDivInfo);
+}
+
+function getSexClass(d) {
+	if (d.Sex==="Girl" || d.Sex==="F") {
+		return "girl";
+	}
+	else if (d.Sex==="Boy" || d.Sex==="M") {
+		return "boy";
+	}
+	else {
+		return "both";	
+	}
 }
